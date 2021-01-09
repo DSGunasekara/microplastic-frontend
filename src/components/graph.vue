31ebcec9362c0fa989e0e823ffdeebe1c9fbfa6e
Dilain Gunasekara <adsgunasekara18@gmail.com>
2021-01-09 15:12:08 +0530

    Graph added

---
+++ src/components/graph.vue
@@ -0,0 +1,37 @@
+
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  data: () => ({
+    chartdata: {
+      labels: ['1ft', '2ft', '3ft', '4ft', '5ft', '6ft', '7ft', '8ft', '9ft', '10ft', '11ft', '12ft', '13ft'],
+      datasets: [
+        {
+          label: 'Micro Plastic',
+          borderColor: '#f87979',
+          data: [46, 45, 32, 42, 36, 30, 27, 26, 24, 29, 22, 21, 12]
+        },
+        {
+          label: 'Plastic Thread',
+          borderColor: '#1fd0b3',
+          data: [25, 20, 12, 15, 12, 10, 11, 7, 3, 5, 1, 1, 0]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import graph from "@/components/graph";
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/graph',
+    name: 'Graphs',
+    component: graph
+  }
 ]
 
 const router = new VueRouter({
